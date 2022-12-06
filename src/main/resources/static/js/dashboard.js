e4ec6c8f1196cd9ed47661252a5826e8cbc1be21
SrenhrKEA <soer53k0@stud.kea.dk>
2022-12-06 21:47:30 +0100

    Added: -working bootstrap tabs for damage report and data registration

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -51,12 +51,3 @@
     }
   })
 })()
-
-function toggleDiv(id) {
-  var x = document.getElementById(id);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
